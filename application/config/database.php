ac229e8bc5bcab7b7c78b8808ef8b017a34dbf4d
Joseph Brower <jbrower@jbrower-Bonobo-Professional.(none)>
2012-03-27 16:17:07 -0600

    fixed db config

--- application/config/database.php
+++ application/config/database.php
@@ -47,11 +47,11 @@
 
 $active_record = TRUE;
  
-$db['production']['hostname'] = "tunnel.pagodabox.com";
-$db['production']['username'] = "shanae";
-$db['production']['password'] = "rabVwD3y";
-$db['production']['database'] = "BeeHive";
-$db['production']['port']     = "3307";
+$db['production']['hostname'] = $_SERVER['DB1_HOST'];
+$db['production']['username'] = $_SERVER['DB1_USER'];
+$db['production']['password'] = $_SERVER['DB1_PASS'];
+$db['production']['database'] = $_SERVER['DB1_NAME'];
+$db['production']['port']     = $_SERVER['DB1_PORT'];
 $db['production']['dbdriver'] = "mysql";
 $db['production']['dbprefix'] = "";
 $db['production']['pconnect'] = TRUE;
