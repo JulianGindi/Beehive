cf863c52d7a263a1d7e73887112f66b81f3d6fa8
Julian Gindi <julian25@gmail.com>
2012-04-04 15:51:54 -0400

    pagoda database fix

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/BeeHive';
+$config['base_url']	= 'http://beehive.pagodabox.com/';
 
 /*
 |--------------------------------------------------------------------------
